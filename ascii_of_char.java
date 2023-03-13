5784d2222357aab92451f0159058bc4eff998dc6
Mani <maniinnovator26@gmail.com>
2023-03-13 15:32:04 +0530

    java training

--- ascii_of_char.java
+++ ascii_of_char.java
@@ -1,8 +1,8 @@
 import java.util.*;
-public class char_input{
+public class ascii_of_char{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         char a=sc.next().charAt(0);
-        System.out.printf("a="+a);
+        System.out.printf("ASCII ="+(int)a);
     }
 }
