1552f3e819239c68926079dbf1cfb3e613401bf6
Mani <maniinnovator26@gmail.com>
2023-03-14 14:01:23 +0530

    Day2 1st commit

---
+++ day2/ArithOperation.java
@@ -0,0 +1,10 @@
+// take one float input from the user and print that value in two decimal places
+import java.util.*;
+public class ArithOperation{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("a+b = "+ (a+b)+" a-b = "+ (a-b)+" a*b = "+ (a*b)+" a/b = "+ (a/b)+" a mod b = "+ (a%b) );
+    }
+}
