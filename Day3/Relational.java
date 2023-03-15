7d24ee02958caee8eb3b4eb0ddd88d0d2c4e2487
Mani <maniinnovator26@gmail.com>
2023-03-15 16:05:40 +0530

    Day 3 completed 3

---
+++ Day3/Relational.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Relational{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==b){
+            System.out.println(a+" and "+b+" are equal");
+        }
+        else{
+        String c=(a+" greater than "+b);
+        String d=(a+" less than "+b);
+        String k=(a>b)?c:d;
+        System.out.println(k);
+        }
+    }
+    }
