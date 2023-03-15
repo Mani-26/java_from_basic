755abec2a28d8de6c4e90cea23f6927367b8032b
Mani <maniinnovator26@gmail.com>
2023-03-15 12:06:05 +0530

    finished all assignments

---
+++ day2/Alice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Alice{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int bird=sc.nextInt();
+        int a=(bird / 10);
+        int b=(bird % 10);
+        System.out.println("Alice must go in path-"+(a+b));
+    }
+}
---
+++ day2/Profit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Profit{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+            int x=sc.nextInt();
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int profit=((a-b)*x-100);
+            System.out.println("The profit obtained is Rs."+profit);
+    }
+}
