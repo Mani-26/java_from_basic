6e3cb53da0c75004df11e32169f8dbb98c8244ad
Mani <maniinnovator26@gmail.com>
2023-03-15 15:38:53 +0530

    Day 3 completed 2

--- day2/extra.java
+++ day2/extra.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Profit{
+public class extra{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in); 
             int input=sc.nextInt();
---
+++ day2/extra2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class extra2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+            float item1=sc.nextFloat();
+            float item2=sc.nextFloat();
+            int dispt=sc.nextInt();
+            float total=(item1+item2);
+            float temp=total*dispt/100;
+            float disam=total-temp;
+            float ams=total-disam;
+            System.out.println("TotalPrice="+total+"\n"+"DiscountedPrice="+disam+"\n"+"Amountsaved="+ams);
+            }
+}
