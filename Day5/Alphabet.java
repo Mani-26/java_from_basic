70ca76d9bbea7e22903a2b86b4fd74b8060f4aa7
Mani <maniinnovator26@gmail.com>
2023-03-17 12:22:38 +0530

    12'o clock commit

---
+++ Day5/Alphabet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringInput{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String in=sc.next();
+        if(in==a ||in==e||in==i||in==o||in==u){
+        System.out.println("Vowel");
+        }
+        System.out.println();
+    }
+}
--- Day5/LabAloc3.java
+++ Day5/LabAloc3.java
@@ -1,18 +1,18 @@
 import java.util.*;
-public class LabAloc2{
+public class LabAloc3{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         int y=sc.nextInt();
         int z=sc.nextInt();
         int n=sc.nextInt();
-        if(n<x && n<y && n<z){
+        if(n<=x && n<=y && n<=z){
                 System.out.println("3");
         }
-        else if(n<x && n<y || n<y && n<z ||n<x && n<z){
+        else if(n<=x && n<=y || n<=y && n<=z ||n<=x && n<=z){
                 System.out.println("2");
         }
-        else if(n<x || n<y || n<z){
+        else if(n<=x || n<=y || n<=z){
                 System.out.println("1");
         }
         else{
