e4db605011c85dec7d44585189e27c95d6c345a7
Mani <maniinnovator26@gmail.com>
2023-03-17 15:56:06 +0530

    Budget&traffic added

---
+++ Day5/Budget.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Budget{
+public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double Blast_Rifle=350.34;
+        double Visual_Sensors=230.90;
+        double Auditory_Sensors=190.55;
+        double Arms=125.30;
+        double Legs=180.90;
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e=sc.nextInt();
+        double budget=((a*Blast_Rifle)+(b*Visual_Sensors)+(c*Auditory_Sensors)+(d*Arms)+(e*Legs));
+        if(budget<=15000){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
---
+++ Day5/Traffic.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Traffic{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    float s=sc.nextFloat();//speed
+    float d=sc.nextFloat();//distance
+    float t=sc.nextFloat();//signal time
+    float time=d/s*3600;
+    if (time<=t){
+     System.out.println("Yes");
+    }
+    else {
+     System.out.println("No");
+     }
+    }
+}
