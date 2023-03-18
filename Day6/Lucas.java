e2d465a86c71caf42255bd4bf774d05cfe47f70c
Mani <maniinnovator26@gmail.com>
2023-03-18 20:32:54 +0530

    Day 6 7th commit

---
+++ Day6/Lucas.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Lucas{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int inp=sc.nextInt();
+    int a=0,b=0,c=1;
+    System.out.println();
+}
+}
