84f2b22409a2e32ca3b01d36bc4b502b3a41b41d
Mani <maniinnovator26@gmail.com>
2023-03-18 16:14:29 +0530

    Day 6 5th commit

---
+++ Day6/Amoeba.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Amoeba{
+public static void main(String args[]){
+    //0,1,1,2,3,5,8,13,21,34,55,89
+    Scanner sc=new Scanner(System.in);
+    int mth=sc.nextInt();
+    int a=0,b=1,c=a+b,i;
+    if(mth==1){
+        System.out.println(a);
+    }
+    else if(mth==2){
+        System.out.println(b);
+    }
+    else if(mth==3){
+        System.out.println(c);
+    }
+    else {
+        for(i=4;i<=mth;i++){
+        a=b;
+        b=c;
+        c=a+b;    
+    }
+    System.out.println(c);
+    }
+}
+}
