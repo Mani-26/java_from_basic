1bac40584fd29c1c9a0a9f61e0e915309199b6b2
Mani <maniinnovator26@gmail.com>
2023-03-20 16:29:48 +0530

    day 7 4th

---
+++ Day6/Target.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Kaprekar{
+public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    int score = sc.nextInt();
+    int mark=0,count=0;
+    while(score<=100){
+        int mark = sc.nextInt();
+        int a=mark;
+        count++;
+    }
+}
+}
