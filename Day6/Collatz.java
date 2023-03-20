3244e81ccbdf2d4957683c1f6ed852ea9008d6c1
Mani <maniinnovator26@gmail.com>
2023-03-20 09:37:39 +0530

    Day 7 1st

---
+++ Day6/Collatz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Collatz{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int inp=sc.nextInt();
+    int count=0;
+    System.out.println(inp);
+    if(inp!=1){
+    while(inp!=1){
+         if(inp%2==0){
+            inp/=2;
+            count++;
+            System.out.println(inp);
+        }
+        else{
+            inp=3*inp+1;
+            count++;
+            System.out.println(inp);
+        }
+    }
+            System.out.println(count);
+    }
+    else{
+        System.out.println(count);
+    }
+}
+}
--- Day6/Lucas.java
+++ Day6/Lucas.java
@@ -3,7 +3,15 @@ public class Lucas{
 public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     int inp=sc.nextInt();
-    int a=0,b=0,c=1;
-    System.out.println();
+    int a=0,b=0,c=1,d=a+b+c;
+    System.out.print(a+" "+b+" "+c);
+    while(inp>3){
+        d=a+b+c;
+        System.out.print(" "+ d);
+        inp--;
+        a=b;
+        b=c;
+        c=d;
+    }
 }
 }
