639c4c0ae1393c872723b14dbb1f17fba31ee8ff
Mani <maniinnovator26@gmail.com>
2023-03-20 19:06:56 +0530

    day 7 5th

--- Day6/Target.java
+++ Day6/Target.java
@@ -1,13 +1,16 @@
 import java.util.*;
-public class Kaprekar{
+public class Target{
 public static void main(String args[]){
     Scanner sc =new Scanner(System.in);
     int score = sc.nextInt();
-    int mark=0,count=0;
-    while(score<=100){
-        int mark = sc.nextInt();
-        int a=mark;
+    int a,count=1;
+    int mark = sc.nextInt();
+    a=mark;
+    while(a<score){
         count++;
+        mark = sc.nextInt();
+        a = a + mark;
     }
+    System.out.println("The number of turns is "+count);
 }
 }
