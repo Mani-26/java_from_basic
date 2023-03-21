27f510e197ad2113bd27d1a5394f67f180c784d8
Mani <maniinnovator26@gmail.com>
2023-03-21 15:03:30 +0530

    day 8 series 7

---
+++ Day8/Series7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Series7{
+public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=1;
+        float b=2,c=3;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                System.out.print(a+" ");
+            }
+            else if(i%2==0){
+                System.out.print(b+" ");
+                b*=3.0f;
+            }
+            else if(i>1 && i%2!=0){
+                System.out.print(c+" ");
+                c*=3.0f;
+            }
+        }
+}
+}
