8dafae985ee4869d308305923a4e0e290f277f47
Mani <maniinnovator26@gmail.com>
2023-03-22 07:25:03 +0530

    Day 9 2nd

---
+++ Day9/Series15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Series15{
+public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=3,b=8,c;
+            System.out.print(a+" "+b+" ");
+        for(int i=2;i<=n+1;i++){
+            c=(a+b)+i;
+            a=b;
+            b=c;
+            System.out.print(c+" ");
+        }
+}
+}
