a8130b261739c7f63b57b4ec8e7d68a28d2f6af0
Mani <maniinnovator26@gmail.com>
2023-03-24 14:22:55 +0530

    MagicSquare2 delete

--- Day11/Mode.java
+++ Day11/Mode.java
@@ -1,12 +1,10 @@
 import java.util.*;
-class Main{
-
-public static void main(String args[])
-{
+public class Mode{
+public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
+    System.out.println("Enter the number of elements in the array");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
         System.out.println("Enter the elements in the array");
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
