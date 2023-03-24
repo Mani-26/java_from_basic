d18cb694ac81bf0fd2a389a4bcf150d6aeadba3a
Mani <maniinnovator26@gmail.com>
2023-03-24 10:57:17 +0530

    Day 11 2nd

---
+++ Day11/Median.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Median{
+public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the  elements in the array");
+        for(int i=0;i<n;i++){
+           a[i]=sc.nextInt();
+        }
+        if(n%2==0){
+            System.out.printf("The median of the array is %.2f"+" ",(float)(a[n/2]+a[(n/2)+1])/2);
+        }else{
+              System.out.printf("The median of the array is %.2f"+" ",(float)(a[(n+1)/2]));
+        }
+}
+}
